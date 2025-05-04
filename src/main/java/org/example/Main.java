d81c235b0a69c305d76b6e2c0669fd76bb01226e
Mikhaylov Igor <orderhaos@yandex.ru>
2025-05-04 16:46:39 +0500

    add function for counting the length of a string

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Длина текста: " + text.length());
     }
 }
