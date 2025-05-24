3ba3e3c1e9b55e0984d53e6ce75f45bf9116c504
Mikhaylov Igor <orderhaos@yandex.ru>
2025-05-24 20:56:27 +0500

    added reading from the scanner and checking for file availability

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,30 @@
 package org.example;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+
+        int count = 1;
+
+        while (true) {
+            System.out.print("Введите путь к файлу : ");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (fileExists && isDirectory) {
+                count++;
+                System.out.println("Путь указан верно. Это файл номер " + count);
+                continue;
+            }
+            else {
+                System.out.println("Файл не существует или указан путь к папке, а не к файлу.");
+
+            }
+        }
     }
 }
